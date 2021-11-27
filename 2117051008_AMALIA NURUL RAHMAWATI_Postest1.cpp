4a1c4fead643ee34449a2b228abf1ec9d50a1b18
amalianurulrahmawati <92409385+amalianurulrahmawati@users.noreply.github.com>
2021-11-27 20:28:41 +0700

    Add files via upload

---
+++ 2117051008_AMALIA NURUL RAHMAWATI_Postest1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main (){
+	string variabel_nama;
+	string variabel_npm;
+	string variabel_alamat;
+	
+		//input
+		getline(cin,variabel_nama);
+		getline(cin,variabel_npm);
+		getline(cin,variabel_alamat);
+		
+		//output
+		cout<<" Nama Saya Yaitu "<<variabel_nama<<endl;
+		cout<<" Dengan NPM "<<variabel_npm<<endl;
+		cout<<" dan Alamat saya di "<<variabel_alamat<<endl;
+		
+		
+		cout<<" >\\\\< "<<endl;
+		cout<<" Amalia Nurul Rahmawati";
+		
+		return 0;
+}
